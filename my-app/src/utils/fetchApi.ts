dfb7673cd2053f6272a79b77bb0049f2309ff79c
neyasno <65032545+neyasno@users.noreply.github.com>
2025-02-12 11:12:19 +0500

    create / fetchApi

---
+++ my-app/src/utils/fetchApi.ts
@@ -0,0 +1,42 @@
+
+type RequestMethod = "GET" | "POST" | "PUT" | "DELETE"
+
+const fetchApi = async ( path : string , method : RequestMethod , body = {} )=>{
+
+    const token = localStorage.getItem('token')
+
+    let response : Response
+
+    if(method === "GET"){
+        response = await fetch( path , 
+            {
+                method , 
+                headers : {
+                    authorization : `Bearer ${token}`
+                },
+            }
+        );
+    }
+    else{
+        response = await fetch( path , 
+            {
+                method , 
+                headers : {
+                    authorization : `Bearer ${token}`
+                },
+                body : JSON.stringify(body)
+            }
+        );
+    }
+
+    
+
+    if(!response.ok){
+        throw new Error("Fetch Error:");
+    }
+    
+    return response.json()
+};
+
+
+export default fetchApi
