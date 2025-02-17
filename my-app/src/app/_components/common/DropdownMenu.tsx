c8ff8041db78af018e69d5a3a3e33cb42d41eb5f
Konstantin <krip.1.101@gmail.com>
2025-02-17 12:19:22 +0500

    fix / header dropdown

--- my-app/src/app/_components/common/DropdownMenu.tsx
+++ my-app/src/app/_components/common/DropdownMenu.tsx
@@ -3,23 +3,19 @@ import { ReactNode } from "react";
 type DropdownProps = {
     enabled?: boolean
     children: ReactNode
-    className?: string
 };
 
-export default function DropdownMenu({children, enabled, className}: DropdownProps) {
+export default function DropdownMenu({children, enabled}: DropdownProps) {
+
     return (
         <>
-            {enabled && enabled === true &&
-                <div className="absolute right-0 z-10 
-                                me-1 mt-2
-                                border-solid border-white border-2
-                                ">
-                    <div className={className}>
+            {enabled && (
+                <div className="absolute right-0">
+                    <div className="mt-2 bg-slate-700 border rounded-md shadow-lg">
                         {children}
                     </div>
                 </div> 
-            }
-
+            )}
         </>        
     );
 }
--- my-app/src/app/_components/layout/Header/Header.tsx
+++ my-app/src/app/_components/layout/Header/Header.tsx
@@ -12,43 +12,46 @@ export default function Header() {
 
   const [enableDropdown, setEnableDropdown] = useState(false);
   const isUserLogined = useAppSelector(state=>state.user.isLogined);
+
   const router = useRouter();
 
   return (
     <div className='bg-header
                     w-full
-                    flex flex-row flex-nowrap
+                    flex flex-nowrap
                     justify-between
                     px-2 py-1
                     '>
       
-      <div className='flex flex-row flex-nowrap 
+      <div className='flex flex-nowrap 
                       gap-x-2'>
         
         <div className='text-2xl my-auto'>TaskBoard</div>
 
-        {/* <div className='flex flex-row flex-nowrap h-4/5 my-auto'>
+        {/* <div className='flex flex-nowrap h-4/5 my-auto'>
           <Button text='Home' handleClick={()=>{}}/>
           <Button text='Boards' handleClick={()=>{}}/>
           <Button text='Other' handleClick={()=>{}}/>
         </div> */}
 
       </div>
+
+      { isUserLogined && (
+        <div className='relative inline-block'
+          onMouseEnter={() => setEnableDropdown(true)}
+          onMouseLeave={() => setEnableDropdown(false)}
+        >
+
+          <Image alt='profile-img' src={'/globe.svg'} width={48} height={48}/>
+          <DropdownMenu enabled={enableDropdown}>
+            <div className='w-28 p-2'>
+              <Button text='Logout' handleClick={()=>{console.log("EXIT"); router.push(ERoutes.EXIT)}}/>
+            </div>
+          </DropdownMenu>
+            
+        </div>
+      )}
       
-      <div>
-        <Image alt='profile-img' src={'/globe.svg'} width={48} height={48} onClick={()=>setEnableDropdown(!enableDropdown)}/>
-        <DropdownMenu enabled={enableDropdown}>
-          <div className='w-28'>
-
-            {isUserLogined? 
-              <Button text='Logout' handleClick={()=>{console.log("EXIT"); router.push(ERoutes.EXIT)}}></Button>
-              :
-              <Button text='Login' handleClick={()=>{console.log("LOGIN");  router.push(ERoutes.LOGIN)}}></Button>
-            }
-
-          </div>
-        </DropdownMenu>
-      </div>
     </div>
   )
 }
