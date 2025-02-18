689f153380caf135b76b08ef9118bd75a0920f80
neyasno <65032545+neyasno@users.noreply.github.com>
2025-02-18 14:22:07 +0500

    create / basic dropdown

--- my-app/src/app/_components/main/TaskBoard/TaskBoardContent/TaskContainer/Task.tsx
+++ my-app/src/app/_components/main/TaskBoard/TaskBoardContent/TaskContainer/Task.tsx
@@ -13,13 +13,12 @@ export default function Task({title , isCompleted , id} : TaskProps) {
     const taskNode = <TaskElement id='' title='task' isCompleted={false}/>;
     const skeletonNode = <div className='flex p-3 items-center gap-2 bg-gray-700 rounded-lg'></div>
 
-    const [isDragged , setIsDragged] = useState(false);
     const [node , setNode] = useState<ReactNode>(taskNode);
 
     const handleDragStart = ( e : React.DragEvent<HTMLLIElement>) => {
-        e.preventDefault()
+        e.dataTransfer.setData('taskID' , id)
         console.log("DRAG_START")
-        setNode(skeletonNode)
+        
     }
 
     const handleDragEnd = (e :  React.DragEvent<HTMLLIElement>) =>{
--- my-app/src/app/_components/main/TaskBoard/TaskBoardContent/TaskContainer/TaskContainer.tsx
+++ my-app/src/app/_components/main/TaskBoard/TaskBoardContent/TaskContainer/TaskContainer.tsx
@@ -34,28 +34,25 @@ export default function TaskContainer({title} : TaskContainerProps) {
 
   const [tasks, setTasks] = useState<TaskProps[]>(testData);
 
-  const handleDragOver = (e: React.DragEvent<HTMLUListElement>) => {
+  const handleDragOver = (e: React.DragEvent<HTMLLIElement>) => {
+    console.log("over")
     e.preventDefault();
   };
 
   const handleDrop = (e : React.DragEvent<HTMLLIElement>) => {
     e.preventDefault();
-    console.log(e.dataTransfer)
+    console.log("DROPP")
     const taskId = e.dataTransfer.getData("taskId");
-    if (!taskId) return;
+    //if (!taskId) return;
 
-    setTasks((prev) => {
-      const taskExists = prev.some((task) => task.id === taskId);
-      if (!taskExists) {
-        return [...prev, { id: taskId, title: `Task ${taskId}`, isCompleted: false }];
-      }
-      return prev;
-    });
+    setTasks([...tasks , {id : "haha" , isCompleted : false , title: " new"}]);
   };
 
   return (
     <li className='flex flex-col bg-gray-400 gap-3 px-3 w-full rounded-lg ' onDrop={handleDrop} onDragOver={handleDragOver}>
-        <div className='pl-2'><TaskContainerHeader title={title}/></div>
+        <div className='pl-2'>
+            <TaskContainerHeader title={title}/>
+        </div>
         <ul className='flex flex-col gap-2 bg-gray-500 rounded-lg mb-3'>
             {tasks.map( (t,index) => <Task id={t.id} isCompleted={t.isCompleted} title={t.title} key={index}/>)}
         </ul>
