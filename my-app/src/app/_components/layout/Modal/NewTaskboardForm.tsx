eaf3e6866efd2ff64df4e44d98e2316919aa3e75
Konstantin <krip.1.101@gmail.com>
2025-02-21 19:20:55 +0500

    add / new taskboard form

--- my-app/src/app/_components/layout/Modal/NewTaskboardForm.tsx
+++ my-app/src/app/_components/layout/Modal/NewTaskboardForm.tsx
@@ -1,7 +1,58 @@
+import { useState } from "react";
+import TextInput from "../../common/TextInput";
+import Button from "../../common/Button";
+import { EApi } from "@/enums";
+import fetchApi from "@/utils/fetchApi";
+import { useAppDispatch } from "@/store/store";
+import { ModalType, setModalType } from "@/store/slices/modalSlice";
+
+
 export default function NewTaskboardForm({}) {
+
+    const dispatcher = useAppDispatch();
+
+    const [title, setTitle] = useState('');
+    const [users, setUsers] = useState<{id: number, email: string}[]>([]);
+    
+    const addUser = () => {
+        setUsers(prev => {
+            const lastId = prev.length > 0 ? (prev[prev.length - 1].id + 1) : 0;
+            return [...prev, {id: lastId, email: ""}];
+        });
+    };
+
+    const changeUser = (id: number, email: string) =>{
+        setUsers(prev => 
+            prev.map(user => 
+                user.id === id ? { ...user, email } : user
+            )
+        );
+    };
+
+    const createHandler = () => {
+        try{
+            fetchApi(EApi.TASKBOARDS, "POST", {
+                title: title,
+                users: users
+            });
+
+        } catch(e){
+            console.log(e);  
+        } finally{
+            dispatcher(setModalType(ModalType.None));
+        }
+    };
+
     return <>
         <div>
-            TEST
+            <TextInput placeholder="Title" value={title} handleChange={setTitle}/>
+
+            <div>
+                {users.map((user) => <TextInput key={user.id} value={user.email} placeholder="email" handleChange={(text)=>changeUser(user.id, text)}/>)}
+                <Button text="Add User" handleClick={addUser} />
+            </div>
+
+            <Button text="Create" handleClick={createHandler}/>
         </div>
     </>;
 }
