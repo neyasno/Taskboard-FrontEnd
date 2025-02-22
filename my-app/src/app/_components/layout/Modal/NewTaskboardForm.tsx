5428596de77d99d7bd60794708dd285d92bb6849
Konstantin <krip.1.101@gmail.com>
2025-02-22 13:42:15 +0500

    upd / NewTaskBoardForm styles added

--- my-app/src/app/_components/layout/Modal/NewTaskboardForm.tsx
+++ my-app/src/app/_components/layout/Modal/NewTaskboardForm.tsx
@@ -5,9 +5,12 @@ import { EApi } from "@/enums";
 import fetchApi from "@/utils/fetchApi";
 import { useAppDispatch } from "@/store/store";
 import { ModalType, setModalType } from "@/store/slices/modalSlice";
+import { useTranslations } from "next-intl";
 
 
 export default function NewTaskboardForm({}) {
+
+    const t = useTranslations('components.main.taskboard.forms');
 
     const dispatcher = useAppDispatch();
 
@@ -29,9 +32,15 @@ export default function NewTaskboardForm({}) {
         );
     };
 
-    const createHandler = () => {
+    const removeUser = (id: number) => {
+        setUsers(prev =>
+            [...prev].filter(x=>x.id!=id)
+        );
+    };
+
+    const createHandler = async () => {
         try{
-            fetchApi(EApi.TASKBOARDS, "POST", {
+            await fetchApi(EApi.TASKBOARDS, "POST", {
                 title: title,
                 users: users
             });
@@ -44,20 +53,31 @@ export default function NewTaskboardForm({}) {
     };
 
     return <>
-<<<<<<< HEAD
         <div>
-            <TextInput placeholder="Title" value={title} handleChange={setTitle}/>
+            <TextInput placeholder={t("title")} value={title} handleChange={setTitle}/>
 
-            <div>
-                {users.map((user) => <TextInput key={user.id} value={user.email} placeholder="email" handleChange={(text)=>changeUser(user.id, text)}/>)}
-                <Button text="Add User" handleClick={addUser} />
+            <div className='ps-5 py-2'>
+                {users.map((user) => <div key={user.id} className="flex items-center">
+                    <span className="w-full">
+                        <TextInput key={user.id} value={user.email} placeholder={t("email")} handleChange={(text)=>changeUser(user.id, text)}/>
+                    </span>
+
+                    <button className="w-10 h-10 border-2 border-solid rounded-full" onClick={()=>removeUser(user.id)}>-</button>
+                </div>)}
+
+                <div className="flex items-center">
+                    
+                    <span className="w-full shadow-inner [box-shadow:inset_0_-25px_15px_rgba(0,0,0,0.4);]">
+                        <TextInput value="" placeholder={t("add_user")} handleChange={()=>{}} readonly/>
+                    </span>
+                    <button className="w-10 h-10 border-2 border-solid rounded-full" onClick={addUser}>+</button>
+
+                </div>
             </div>
-
-            <Button text="Create" handleClick={createHandler}/>
-=======
-        <div >
-            TEST
->>>>>>> f783bb3dc9d387ef71b14c52f24bd5311edde7a2
+            
+            <div className="mt-5">
+                <Button text={t("create_taskboard")} handleClick={createHandler}/>
+            </div>
         </div>
     </>;
 }
