ec662dfb261d76c3f07be67989f2ceb075fd0b71
Konstantin <krip.1.101@gmail.com>
2025-02-22 13:41:24 +0500

    upd / TextInput readonly flag added

--- my-app/src/app/_components/common/TextInput.tsx
+++ my-app/src/app/_components/common/TextInput.tsx
@@ -5,13 +5,16 @@ type TextInputProps = {
   value: string
   placeholder: string
   isPassword?: boolean
+  readonly?: boolean
 }
 
-export default function TextInput({ value, placeholder, isPassword, handleChange }: TextInputProps) {
+export default function TextInput({ value, placeholder, isPassword, readonly, handleChange }: TextInputProps) {
   return (
     <input 
       type={isPassword ? "password" : "text"}
       
+      readOnly={readonly}
+
       className='px-3 py-2 border-2 w-full bg-transparent
                       border-black dark:border-white'
 
